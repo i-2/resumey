53244bab6afe1433b73ee8fefc3b931b75b8df3c
sourcepirate <E0267819@u.nus.edu>
2021-01-06 19:18:38 +0800

    updated failed tests

--- frontend/__tests__/ProfessionalSummary.test.tsx
+++ frontend/__tests__/ProfessionalSummary.test.tsx
@@ -1,10 +1,10 @@
 import * as  React from 'react';
 import * as renderer from 'react-test-renderer';
-import {ProfessionalSummary } from '../components/ProfessionalSummary';
+import { ProfessionalSummary } from '../components/ProfessionalSummary';
 
 it('renders completed correctly', () => {
   const tree = renderer
-    .create(<ProfessionalSummary />)
+    .create(<ProfessionalSummary name="" title="" initValues={{}} onNext={() => { }} onPrev={() => { }} submitValues={(v) => { }} />)
     .toJSON();
   expect(tree).toMatchSnapshot();
 });
